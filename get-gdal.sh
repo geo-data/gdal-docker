d572ae31ceffadc6d79014c15e968dc66821f547
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2015-04-14 23:55:53 +0100

    Ensure the master branch builds an image from gdal trunk
    This means that the geodata/gdal:latest image will contain code from gdal trunk.

--- get-gdal.sh
+++ get-gdal.sh
@@ -8,4 +8,4 @@ checkout=`cat /tmp/gdal-checkout.txt`
 
 # Checkout GDAL from github
 cd / && \
-    svn checkout "https://svn.osgeo.org/gdal/${checkout}/" /tmp/gdal || exit 1
+    svn checkout --quiet "https://svn.osgeo.org/gdal/${checkout}/" /tmp/gdal || exit 1
